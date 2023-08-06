79f768297abedae9b98bf3d22cac51faaaf6fdba
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-08-06 17:29:35 -0500

    todo-list-ex[ress

---
+++ server.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const PORT = 3000;
+require('dotenv').config();
+
+let db,
+  dbConnectionStr = process.env.DB_STRING,
+  dbName = 'todo';
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+  .then(client => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  })
+  .catch(error => {
+    console.error(error);
+    process.exit(1); // Exit the application on connection error
+  });
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', async (request, response) => {
+  try {
+    const todoItems = await db.collection('todos').find().toArray();
+    const itemsLeft = await db.collection('todos').countDocuments({ completed: false });
+    response.render('index.ejs', { items: todoItems, left: itemsLeft });
+  } catch (error) {
+    console.error(error);
+    response.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/addTodo', (request, response) => {
+  db.collection('todos').insertOne({ thing: request.body.todoItem, completed: false })
+    .then(result => {
+      console.log('Todo Added');
+      response.redirect('/');
+    })
+    .catch(error => {
+      console.error(error);
+      response.status(500).send('Internal Server Error');
+    });
+});
+
+app.put('/markComplete', (request, response) => {
+  db.collection('todos').updateOne(
+    { thing: request.body.itemFromJS },
+    {
+      $set: {
+        completed: true
+      }
+    },
+    {
+      sort: { _id: -1 },
+      upsert: false
+    }
+  )
+    .then(result => {
+      console.log('Marked Complete');
+      response.json('Marked Complete');
+    })
+    .catch(error => {
+      console.error(error);
+      response.status(500).send('Internal Server Error');
+    });
+});
+
+app.put('/markUnComplete', (request, response) => {
+  db.collection('todos').updateOne(
+    { thing: request.body.itemFromJS },
+    {
+      $set: {
+        completed: false
+      }
+    },
+    {
+      sort: { _id: -1 },
+      upsert: false
+    }
+  )
+    .then(result => {
+      console.log('Marked Uncomplete');
+      response.json('Marked Uncomplete');
+    })
+    .catch(error => {
+      console.error(error);
+      response.status(500).send('Internal Server Error');
+    });
+});
+
+app.delete('/deleteItem', (request, response) => {
+  db.collection('todos').deleteOne({ thing: request.body.itemFromJS })
+    .then(result => {
+      console.log('Todo Deleted');
+      response.json('Todo Deleted');
+    })
+    .catch(error => {
+      console.error(error);
+      response.status(500).send('Internal Server Error');
+    });
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
